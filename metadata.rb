3cad7edde639c4bf9e2753139745f9ecf9d4790c
Andrew Hollingsworth <adh@techopsguru.com>
2013-04-16 09:57:39 -0700

    Vagrantfile for vagrant 1.1, thor and supporting files

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Sander van Zoest"
 maintainer_email "sander@vanzoest.com"
 license          "Apache 2.0"
 description      "System Accounts management"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.3"
 replaces         "sudo"
 conflicts        "sudo"
