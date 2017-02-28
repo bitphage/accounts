2445e1f1c59f4a9418950fa1214e6b355f0bdab0
Vladimir Kamarzin <vvk@vvk.pp.ru>
2017-02-28 16:35:30 +0500

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sander@vanzoest.com'
 license 'Apache 2.0'
 description 'System Accounts management'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
+version '0.2.2'
 replaces 'sudo'
 conflicts 'sudo'
 %w(redhat centos debian ubuntu).each do |os|
